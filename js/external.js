70e3f61468cc6b89de47edd03f0d470484a494a3
William Mejia <willx1995@gmail.com>
2022-01-31 17:01:49 -0600

    Finished JS InlineExcerises and External

--- js/external.js
+++ js/external.js
@@ -12,32 +12,57 @@ if (response) {
   prompt(`What is your favorite color?`);
 }
 
-// let userChoice = confirm("Are you sure?");
+let moviePrice = 3;
 
-// let currentSpeed = Number(prompt("How fast are you driving now?"));
-// console.log(currentSpeed);
-
-// let plannedSpeedIncrease = Number(
-//   prompt("How many more MPH do you want to go?")
-// );
-// console.log(plannedSpeedIncrease);
-
-// // console.log("Your planned speed is " + (currentSpeed + plannedSpeedIncrease));
-
-// let newSpeed = currentSpeed + plannedSpeedIncrease;
-
-// let distanceLeft = Number(
-//   prompt("How many miles are you from your destination?")
-// );
-
-// let timeAtCurrentSpeed = (distanceLeft / currentSpeed) * 60;
-// let timeplanned = (distanceLeft / newSpeed) * 60;
-// let timeGain = timeAtCurrentSpeed - timeplanned;
-
-// alert(
-//   "If you drive at " +
-//     newSpeed +
-//     " mph, you will arrive " +
-//     timeGain.toFixed(2) +
-//     " minutes sooner."
+let littleMermaid = Number(
+  prompt("How many days would you like to rent Little Mermaid?")
 );
+
+let brotherBear = Number(
+  prompt("How many days would you like to rent Brother Bear?")
+);
+
+let hercules = Number(prompt("How many days would you like to rent Hercules?"));
+
+let total = moviePrice * (littleMermaid + brotherBear + hercules);
+
+alert("Your total is : " + "$" + total.toFixed(2));
+
+let googleRate = Number(prompt("How much does Google pay?"));
+let amazonRate = Number(prompt("How much does Amazon pay?"));
+let facebookRate = Number(prompt("How much does FB pay?"));
+
+let googleHours = Number(prompt("How many hours did you work at Google?"));
+let amazonHours = Number(prompt("How many hours did you work at Amazon?"));
+let facebookHours = Number(prompt("How many hours did you work at FB?"));
+
+console.log(googleHours);
+console.log(amazonHours);
+console.log(facebookHours);
+
+let totalIncome =
+  googleHours * googleRate +
+  amazonHours * amazonRate +
+  facebookHours * facebookRate;
+
+alert("Your pay for the week is: " + "$" + totalIncome);
+
+let classNotFull = confirm("Is the class full?");
+let scheduleConflict = confirm("Do you have another class at the same time?");
+
+if (!classNotFull && !scheduleConflict) {
+  alert("Can attend class");
+} else {
+  alert("Cannot attend class");
+}
+
+let premiumMember = confirm("Are you a premium member?");
+let items = prompt("How many items do you want to purchase?");
+
+let discount = premiumMember || items >= 2;
+
+if (discount) {
+  alert("discount applied to cart");
+} else {
+  alert("no discount to cart");
+}
