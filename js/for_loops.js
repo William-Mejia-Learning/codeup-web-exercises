b455ab4089500a93fc43e0d86ab48c5b717bda8c
William Mejia <willx1995@gmail.com>
2022-02-08 08:53:18 -0600

    adding js/for_loops.js

---
+++ js/for_loops.js
@@ -0,0 +1,44 @@
+// Problem 2
+// function showMultiplicationTable(num){
+//     for(let i = 1; i <= 10; i++){
+//         let result =  num * i
+//         console.log(result);
+//     }
+// }
+//
+// showMultiplicationTable(8);
+//
+//
+// Problem 3
+// function tenRandomNumbers(){
+//     for(let i = 1; i <= 10; i++){
+//         let random = Math.floor(Math.random() * 200) + 20;
+//         if(random % 2 === 0){
+//             console.log(random + ' is even');
+//         }else{
+//             console.log(random + ' is odd');
+//         }
+//     }
+// }
+//
+// tenRandomNumbers();
+
+//problem 4
+// function addStr(){
+//     for(let i = 1; i <= 9; i++){
+//         let numStr=[];
+//         for(j = 1; j <= i; j++){
+//             numStr.push(i);
+//         }
+//         console.log(numStr.join(""));
+//     }
+//
+// }
+//
+// addStr();
+
+
+//problem 5
+// for(let i = 100; i >= 5; i -= 5){
+//     console.log(i)
+// }
--- js/while.js
+++ js/while.js
@@ -11,21 +11,21 @@
 //     console.log(number)
 // }while (number < 65536)
 
-let allCones = Math.floor(Math.random() * 50) + 50;
-console.log(allCones)
-
-
-do{
-    let conesSold = Math.floor(Math.random() * 5) + 1;
-    console.log(conesSold)
-    console.log(allCones + ' cones left and ' + conesSold + ' cone sold')
-    // console.log('Cannot sell you ' + conesSold + ' I only have ' + conesLeft)
-    if(conesSold > allCones) {
-        console.log('Cannot sell ' + conesSold +' cones, I only have ' + (allCones) + ' cones left, ' + 'sold ');
-    }else {
-        console.log(conesSold + ' cones sold');
-        allCones = allCones - conesSold
-    }
-}while(allCones > 0);
-console.log('I sold them all')
-
+// let allCones = Math.floor(Math.random() * 50) + 50;
+// console.log(allCones)
+//
+//
+// do{
+//     let conesSold = Math.floor(Math.random() * 5) + 1;
+//     console.log(conesSold)
+//     console.log(allCones + ' cones left and ' + conesSold + ' cone sold')
+//     // console.log('Cannot sell you ' + conesSold + ' I only have ' + conesLeft)
+//     if(conesSold > allCones) {
+//         console.log('Cannot sell ' + conesSold +' cones, I only have ' + (allCones) + ' cones left, ' + 'sold ');
+//     }else {
+//         console.log(conesSold + ' cones sold');
+//         allCones = allCones - conesSold
+//     }
+// }while(allCones > 0);
+// console.log('I sold them all')
+//
